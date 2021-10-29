4eabf5d6915be70af247af8d1fbd7c4233b56ec1
kongeee <furkanekici1997@gmail.com>
2021-10-29 19:24:31 +0300

    menuye kategori ekle

--- menu_operations/addCategoryToMenu.php
+++ menu_operations/addCategoryToMenu.php
@@ -50,15 +50,26 @@ if(!$_POST){
     <form action="" method="POST">
         <?php
         $menu_id = $_POST['menu_id'];
-        $sql = "SELECT * FROM categories INNER JOIN menus INNER JOIN menu_details WHERE categories.category_id=menu_details.category_id AND  menu_details.menu_id='$menu_id'";
+        $sql = "SELECT * FROM categories INNER JOIN menu_details WHERE categories.category_id=menu_details.category_id AND  menu_details.menu_id='$menu_id'";
         $result = $DBconn->query($sql);
         $categoryIdArray = array();
         while($row = mysqli_fetch_assoc($result)){ 
             array_push($categoryIdArray, $row['category_id']);
         }
-        print_r($categoryIdArray);
-        ?>
         
+        $sql2 = "SELECT * FROM categories";
+        $result2 = $DBconn->query($sql2);
+        while($row2 = mysqli_fetch_assoc($result2)){
+            $category_id = $row2['category_id'];
+            $category_name = $row2['category_name'];
+            if(in_array($row2['category_id'], $categoryIdArray)){
+                echo "<input type='checkbox' checked name='$category_id'>$category_name";
+            }else{
+                echo "<input type='checkbox' name='$category_id'>$category_name";
+            }
+        }
+        ?>
+        <input type="hidden" name="menu" value="<?php echo $menu_id ?>">
         <input type="Submit" value="Düzenle">
     </form>
         
@@ -68,20 +79,24 @@ if(!$_POST){
 
 <?php
 }else{
-    $menu_name = $_POST['menu_name'];
+    $menu_id = $_POST['menu'];
+    $newCategories = array_keys($_POST);
+    array_pop($newCategories);
 
-    
-    $sql = "INSERT INTO menu_details (menu_name) VALUES ('$menu_name')";
-    $result = $DBconn->query($sql);
-    
-    
-    if($result === TRUE){
-        echo("Ekleme başarılı");
-        header("Refresh:3; url=../admin_panel/admin_panel.php");
+    //menu_detailsten bu menuyu sil sonra seçilenlere göre hepsini baştan ekle
+    $sql = "DELETE FROM menu_details WHERE menu_id = '$menu_id'";
+    if($DBconn->query($sql) === TRUE){
+        foreach($newCategories as $category_id){
+            $sql = "INSERT INTO menu_details(menu_id, category_id) VALUES ('$menu_id', '$category_id')";
+            $DBconn->query($sql);
+        }
+        echo "Düzenleme başarılı";
     }
     else{
-        echo("HATA");
-        header("Refresh:3; url=addMenu.php");
+        echo "HATA";
     }
+
+    header("Refresh:3; url=../admin_panel/admin_panel.php");
+    
 }
 ?>
